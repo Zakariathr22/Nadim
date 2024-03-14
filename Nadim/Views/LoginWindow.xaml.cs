a5354e3404f078ae0d52e78f8df6b48b88346c9f
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-03-14 16:36:52 +0100

    Started the implementation of a Login window within the application to facilitate user authentication. This window will serve as the entry point for users to log in to the system.

---
+++ Nadim/Views/LoginWindow.xaml.cs
@@ -0,0 +1,91 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT.Interop;
+
+
+namespace Nadim.Views
+{
+    public sealed partial class LoginWindow : Window
+    {
+        private AppWindow appWindow;
+        private OverlappedPresenter overlappedPresenter;
+        private AppWindowTitleBar titleBar;
+        public LoginWindow()
+        {
+            this.InitializeComponent();
+
+            appWindow = GetAppWindowForCurrentWindow();
+            overlappedPresenter = GetAppWindowOverlappedPresenter(appWindow);
+            titleBar = GetAppWindowTitleBar(appWindow);
+
+            appWindow.Resize(new Windows.Graphics.SizeInt32(840, 500));
+
+            overlappedPresenter.IsResizable = false;
+            overlappedPresenter.IsMinimizable = false;
+            overlappedPresenter.IsMaximizable = false;
+
+            titleBar.ExtendsContentIntoTitleBar = true;
+            titleBar.PreferredHeightOption = TitleBarHeightOption.Tall;
+
+            CenterWindow();
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(myWndId);
+        }
+
+        public OverlappedPresenter GetAppWindowOverlappedPresenter(AppWindow appWindow)
+        {
+            return (OverlappedPresenter)appWindow.Presenter;
+        }
+
+        public AppWindowTitleBar GetAppWindowTitleBar(AppWindow appWindow)
+        {
+            if (AppWindowTitleBar.IsCustomizationSupported())
+            {
+                var titleBar = appWindow.TitleBar;
+
+                return titleBar;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void CenterWindow()
+        {
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+            Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            if (appWindow is not null)
+            {
+                Microsoft.UI.Windowing.DisplayArea displayArea = Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(windowId, Microsoft.UI.Windowing.DisplayAreaFallback.Nearest);
+                if (displayArea is not null)
+                {
+                    var CenteredPosition = appWindow.Position;
+                    CenteredPosition.X = ((displayArea.WorkArea.Width - appWindow.Size.Width) / 2);
+                    CenteredPosition.Y = ((displayArea.WorkArea.Height - appWindow.Size.Height) / 2);
+                    appWindow.Move(CenteredPosition);
+                }
+            }
+        }
+    }
+}
