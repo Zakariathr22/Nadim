e0507f2bd2e8309ac4bae1d722f6b363df1b91a2
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-03-28 15:05:17 +0100

    Login -> ViewModel modification (adding observable properties)

--- Nadim/ViewModels/LoginViewModel.cs
+++ Nadim/ViewModels/LoginViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,15 @@ namespace Nadim.ViewModels
 {
     public partial class LoginViewModel: ObservableObject
     {
+        [ObservableProperty] string emailOrPhone = "";
+        [ObservableProperty] string password = "";
+
+
+        [ObservableProperty] private Brush passwordBoxBackground = App.Current.Resources["ControlFillColorDefaultBrush"] as Brush;
+        [ObservableProperty] private Brush emailOrPhoneTextBoxBackground = App.Current.Resources["ControlFillColorDefaultBrush"] as Brush;
+
+        [ObservableProperty] Visibility emailOrPhoneRequiredErrorVisibility = Visibility.Collapsed;
+        [ObservableProperty] Visibility passwordRequiredErrorVisibility = Visibility.Collapsed;
+        [ObservableProperty] Visibility emailOrPhoneOrPasswordIncorrectVisibility = Visibility.Collapsed;
     }
 }
