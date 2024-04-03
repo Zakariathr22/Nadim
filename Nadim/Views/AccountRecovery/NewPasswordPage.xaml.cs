c4395a1473115468fa8d79bfb43167c13f85f6be
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-04-03 03:06:08 +0100

    Account Recovery -> New Password -> View Validation

--- Nadim/Views/AccountRecovery/NewPasswordPage.xaml.cs
+++ Nadim/Views/AccountRecovery/NewPasswordPage.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Nadim.Services;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -30,9 +31,124 @@ namespace Nadim.Views.AccountRecovery
             mainPanel.DataContext = AccountRecoveryWindow.accountRecoveryNewPasswordViewModel;
         }
 
-        private void confirmPasswordBox_Click(object sender, RoutedEventArgs e)
+        private void resetPasswordButton_Click(object sender, RoutedEventArgs e)
         {
             AccountRecoveryWindow.accountRecoveryNewPasswordViewModel.ValdateCommand.Execute(this);
+        }
+
+        private void passwordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            string Password = passwordBox.Password;
+            if (Password == ""
+            || !DataValidationService.HasMinimumCharacters(Password, 8)
+            || !DataValidationService.HasMaximumCharacters(Password, 128)
+            || !Password.Any(char.IsLetter)
+            || !DataValidationService.ContainsSymbol(Password)
+            || !DataValidationService.ContainsNumber(Password))
+            {
+                passwordBox.Background = App.Current.Resources["SystemFillColorCriticalBackgroundBrush"] as Brush;
+                if (Password == "")
+                {
+                    passwordRequiredError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordRequiredError.Visibility = Visibility.Collapsed;
+                }
+
+                if (!DataValidationService.HasMinimumCharacters(Password, 8) && Password != "")
+                {
+                    passwordTooShortError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordTooShortError.Visibility = Visibility.Collapsed;
+                }
+
+                if (!DataValidationService.HasMaximumCharacters(Password, 128))
+                {
+                    passwordTooLongError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordTooLongError.Visibility = Visibility.Collapsed;
+                }
+
+                if (!Password.Any(char.IsLetter) && Password != "")
+                {
+                    passwordMustContainAlpabetError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordMustContainAlpabetError.Visibility = Visibility.Collapsed;
+                }
+                if (!DataValidationService.ContainsSymbol(Password) && Password != "")
+                {
+                    passwordMustContainSymbolError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordMustContainSymbolError.Visibility = Visibility.Collapsed;
+                }
+                if (!DataValidationService.ContainsNumber(Password) && Password != "")
+                {
+                    passwordMustContainNumberError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    passwordMustContainNumberError.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                passwordBox.Background = App.Current.Resources["ControlFillColorDefaultBrush"] as Brush;
+                passwordRequiredError.Visibility = Visibility.Collapsed;
+                passwordTooShortError.Visibility = Visibility.Collapsed;
+                passwordTooLongError.Visibility = Visibility.Collapsed;
+                passwordMustContainAlpabetError.Visibility = Visibility.Collapsed;
+                passwordMustContainSymbolError.Visibility = Visibility.Collapsed;
+                passwordMustContainNumberError.Visibility = Visibility.Collapsed;
+            }
+            if (confirmPasswordBox.Password != passwordBox.Password && confirmPasswordBox.Password != "")
+            {
+                confirmPasswordBox.Background = App.Current.Resources["SystemFillColorCriticalBackgroundBrush"] as Brush;
+                confirmPasswoedMatchError.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                confirmPasswordBox.Background = App.Current.Resources["ControlFillColorDefaultBrush"] as Brush;
+                confirmPasswoedMatchError.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void confirmPasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (confirmPasswordBox.Password == "" || confirmPasswordBox.Password != passwordBox.Password)
+            {
+                confirmPasswordBox.Background = App.Current.Resources["SystemFillColorCriticalBackgroundBrush"] as Brush;
+                if (confirmPasswordBox.Password == "")
+                {
+                    confirmPasswoedRequiredError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    confirmPasswoedRequiredError.Visibility = Visibility.Collapsed;
+                }
+                if (confirmPasswordBox.Password != passwordBox.Password && confirmPasswordBox.Password != "")
+                {
+                    confirmPasswoedMatchError.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    confirmPasswoedMatchError.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                confirmPasswordBox.Background = App.Current.Resources["ControlFillColorDefaultBrush"] as Brush;
+                confirmPasswoedRequiredError.Visibility = Visibility.Collapsed;
+                confirmPasswoedMatchError.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
