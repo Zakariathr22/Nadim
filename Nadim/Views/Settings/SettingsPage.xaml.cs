7e6faa8d820ac3a2d9f6ac609fe533e1dab466ba
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-05-14 09:27:36 +0100

    Settings Page -> UI Functionality (UI Code Implementaion in C# for user interaction when user changes files font family)

--- Nadim/Views/Settings/SettingsPage.xaml.cs
+++ Nadim/Views/Settings/SettingsPage.xaml.cs
@@ -14,6 +14,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI;
 using Nadim.Services;
+using Microsoft.UI.Composition.SystemBackdrops;
+using System.Xml;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -41,6 +43,54 @@ namespace Nadim.Views.Settings
             } else
             {
                 ThemeSelectorService.SetTheme(ElementTheme.Dark);
+            }
+        }
+
+        private void backDropComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (backDropComboBox.SelectedIndex == 0)
+            {
+                App.mainWindow.SystemBackdrop = new MicaBackdrop() { Kind = MicaKind.Base};
+            }
+            else if (backDropComboBox.SelectedIndex == 1)
+            {
+                App.mainWindow.SystemBackdrop = new MicaBackdrop() { Kind = MicaKind.BaseAlt };
+            }
+            else
+            {
+                App.mainWindow.SystemBackdrop = new DesktopAcrylicBackdrop();
+            }
+            
+        }
+
+        private void filesFontFamilyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (fontTestTextBlock != null)
+            {
+                if (filesFontFamilyComboBox.SelectedIndex == 0)
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Times New Roman");
+                } 
+                else if (filesFontFamilyComboBox.SelectedIndex == 1)
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Arial");
+                }
+                else if (filesFontFamilyComboBox.SelectedIndex == 2)
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Calibri");
+                }
+                else if (filesFontFamilyComboBox.SelectedIndex == 3)
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Sakkal Majalla");
+                }
+                else if (filesFontFamilyComboBox.SelectedIndex == 4)
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Traditional Arabic");
+                }
+                else
+                {
+                    fontTestTextBlock.FontFamily = new FontFamily("Times New Roman");
+                }
             }
         }
     }
