7887a1512637f442c9482b8a5a010cc2ed14f264
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-05-14 23:10:13 +0100

    Settings Page -> XAML Desing Implentation (Disable shortcut setting card)

--- Nadim/Views/Settings/SettingsPage.xaml.cs
+++ Nadim/Views/Settings/SettingsPage.xaml.cs
@@ -16,6 +16,10 @@ using Microsoft.UI;
 using Nadim.Services;
 using Microsoft.UI.Composition.SystemBackdrops;
 using System.Xml;
+using IWshRuntimeLibrary;
+using System.Diagnostics;
+using System.Text;
+using System.Runtime.InteropServices;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -92,6 +96,38 @@ namespace Nadim.Views.Settings
                     fontTestTextBlock.FontFamily = new FontFamily("Times New Roman");
                 }
             }
+        }
+
+        private void ShortCutToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+           if (ShortCutToggleSwitch.IsOn)
+            {
+                CreateShortcut();
+            }
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool QueryFullProcessImageName(IntPtr hProcess, uint dwFlags,
+                                        [Out, MarshalAs(UnmanagedType.LPTStr)] StringBuilder lpExeName,
+                                        ref uint lpdwSize);
+        private void CreateShortcut()
+        {
+            // Get a handle to the current process
+            IntPtr hProcess = Process.GetCurrentProcess().Handle;
+            uint dwFlags = 0;
+            StringBuilder lpExeName = new StringBuilder(260);
+            uint lpdwSize = (uint)lpExeName.Capacity;
+            //Get exe path
+            bool result = QueryFullProcessImageName(hProcess, dwFlags, lpExeName, ref lpdwSize);
+
+            object shDesktop = (object)"Desktop";
+            WshShell shell = new WshShell();
+            string shortcutAddress = (string)shell.SpecialFolders.Item(ref shDesktop) + @"\Nadim.lnk";
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "New shortcut for Nadim";
+            shortcut.Hotkey = "Ctrl+Shift+N";
+            shortcut.TargetPath = lpExeName.ToString();
+            shortcut.Save();
         }
     }
 }
