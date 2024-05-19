115e99c674cd9a6fdbae23215876425b038d19d9
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-05-19 21:45:38 +0100

    Main Window -> ViewModel Modification + Design Review (Adding of Info Badge to Account Navigation View Item)

--- Nadim/ViewModels/MainViewModel.cs
+++ Nadim/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Composition.SystemBackdrops;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
+using Nadim.Models;
 using Nadim.Services;
 using System;
 using System.Collections.Generic;
@@ -18,11 +19,66 @@ namespace Nadim.ViewModels
         [ObservableProperty] private int appBackDrop;
         [ObservableProperty] private int landingPage;
 
+        private Office office;
+        private User user;
+
+        [ObservableProperty] string navigationViewPanTitle;
+
+        [ObservableProperty] int homeInfoBadgeValue = 0;
+        [ObservableProperty] Visibility homeInfoBadgeVisibility;
+
+        [ObservableProperty] int clientsInfoBadgeValue = 0;
+        [ObservableProperty] Visibility clientsInfoBadgeVisibility;
+
+        [ObservableProperty] int foldersInfoBadgeValue = 0;
+        [ObservableProperty] Visibility foldersInfoBadgeVisibility;
+
+        [ObservableProperty] int scheduleInfoBadgeValue = 0;
+        [ObservableProperty] Visibility scheduleInfoBadgeVisibility;
+
+        [ObservableProperty] int feesInfoBadgeValue = 0;
+        [ObservableProperty] Visibility feesInfoBadgeVisibility;
+
+        [ObservableProperty] int lawsInfoBadgeValue = 0;
+        [ObservableProperty] Visibility lawsInfoBadgeVisibility;
+
+        [ObservableProperty] int tasksInfoBadgeValue = 0;
+        [ObservableProperty] Visibility tasksInfoBadgeVisibility;
+
+        [ObservableProperty] int notesInfoBadgeValue = 0;
+        [ObservableProperty] Visibility notesInfoBadgeVisibility;
+
+        [ObservableProperty] int comunityInfoBadgeValue = 0;
+        [ObservableProperty] Visibility comunityInfoBadgeVisibility;
+
+        [ObservableProperty] int accountInfoBadgeValue = 0;
+        [ObservableProperty] Visibility accountInfoBadgeVisibility;
+        [ObservableProperty] string accountNavigationViewItemTextBlockText;
         public MainViewModel()
         {
             appTheme = int.Parse(ConfigurationService.GetAppSetting("AppTheme"));
             appBackDrop = int.Parse(ConfigurationService.GetAppSetting("AppBackDrop"));
             landingPage = int.Parse(ConfigurationService.GetAppSetting("LandingPage"));
+
+            office = new Office();
+            office.naming = "مكتب الأستاذ طاهري زكرياء";
+
+            user = new User();
+            user.firstName = "زكرياء";
+            user.lastName = "طاهري";
+
+            HomeInfoBadgeVisibility = SetInfoBarVisbility(HomeInfoBadgeValue);
+            ClientsInfoBadgeVisibility = SetInfoBarVisbility(ClientsInfoBadgeValue);
+            FoldersInfoBadgeVisibility = SetInfoBarVisbility(FoldersInfoBadgeValue);
+            ScheduleInfoBadgeVisibility = SetInfoBarVisbility(ScheduleInfoBadgeValue);
+            FeesInfoBadgeVisibility = SetInfoBarVisbility(FeesInfoBadgeValue);
+            LawsInfoBadgeVisibility = SetInfoBarVisbility(LawsInfoBadgeValue);
+            TasksInfoBadgeVisibility = SetInfoBarVisbility(TasksInfoBadgeValue);
+            NotesInfoBadgeVisibility = SetInfoBarVisbility(NotesInfoBadgeValue);
+            ComunityInfoBadgeVisibility = SetInfoBarVisbility(ComunityInfoBadgeValue);
+            AccountInfoBadgeVisibility = SetInfoBarVisbility(AccountInfoBadgeValue);
+
+            AccountNavigationViewItemTextBlockText = $"الحساب ({user.lastName} {user.firstName})";
         }
 
         public void SetAppTheme(Window window) 
@@ -55,6 +111,13 @@ namespace Nadim.ViewModels
             {
                 window.SystemBackdrop = new DesktopAcrylicBackdrop();
             }
+        }
+
+        private Visibility SetInfoBarVisbility(int value)
+        {
+            if (value > 0)
+            return Visibility.Visible;
+            return Visibility.Collapsed;
         }
     }
 }
