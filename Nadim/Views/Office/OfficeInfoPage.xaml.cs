559f287b41055daebf1d85a751030a9e83cc3a0b
Zakaria Tahri <101801384+Zakariathr22@users.noreply.github.com>
2024-07-22 20:02:50 +0100

    Office -> Office Information Page -> Design -> Design Implementation XAML & C# + Design Review

---
+++ Nadim/Views/Office/OfficeInfoPage.xaml.cs
@@ -0,0 +1,46 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Nadim.Views.Office
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OfficeInfoPage : Page
+    {
+        public OfficeInfoPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void editNamingHyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editHeadquartersButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void editHeadquartersDetailsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
